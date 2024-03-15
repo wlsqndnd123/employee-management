9f960e00a87f2e25a16211ac53034eaed4595a8a
OneStar90 <mrb8215@gmail.com>
2024-03-15 19:05:13 +0900

    Feat: 주석 추가

--- src/view/user/RequestVacation.java
+++ src/view/user/RequestVacation.java
@@ -4,10 +4,9 @@ import com.toedter.calendar.JDateChooser;
 
 import javax.swing.*;
 
-
 /**
- * Desc : 사원의 휴가 신청서 입력을 위한 view
- * 작성자 : 고한별
+ * Desc : 사원의 휴가 신청서 입력을 위한 view<br>
+ * 작성자 : 고한별<br>
  * 작성일 : 2024.03.15
  */
 public class RequestVacation extends JFrame {
@@ -20,6 +19,9 @@ public class RequestVacation extends JFrame {
     private JButton request;
     private JButton cancel;
 
+    /**
+     * Desc : 휴가 신청 main frame 구현
+     */
     public RequestVacation() {
         setTitle("휴가 신청");
         setLayout(null);
@@ -33,6 +35,9 @@ public class RequestVacation extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    /**
+     * Desc : 휴가 시작일과 종료일을 설정하는 영역 생성
+     */
     public void createVacDate(){
         startDateTag = new JLabel("휴가 시작 일자");
         endDateTag = new JLabel("휴가 종료 일자");
@@ -53,6 +58,9 @@ public class RequestVacation extends JFrame {
         add(vacEndDate);
     }
 
+    /**
+     * Desc : 휴가 사유를 입력하는 영역 생성
+     */
     public void createContentsArea(){
         vacContents = new JTextArea();
         contentsPad = new JScrollPane(vacContents);
@@ -62,6 +70,9 @@ public class RequestVacation extends JFrame {
         add(contentsPad);
     }
 
+    /**
+     * Desc : 휴가 신청 및 취소버튼 생성
+     */
     public void createConfirmButton(){
         request = new JButton("신청");
         cancel = new JButton("취소");
