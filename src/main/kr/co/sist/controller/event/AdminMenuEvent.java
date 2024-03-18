684bfa7711b6939f8d31b4ddb496f7b7c6a184fd
OneStar90 <mrb8215@gmail.com>
2024-03-18 18:54:50 +0900

    Feat: AdminMenuEvent 뼈대 구현

--- src/main/kr/co/sist/controller/event/AdminMenuEvent.java
+++ src/main/kr/co/sist/controller/event/AdminMenuEvent.java
@@ -1,4 +1,41 @@
 package kr.co.sist.controller.event;
 
-public class AdminMenuEvent {
+import kr.co.sist.view.admin.AdminMenu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class AdminMenuEvent extends WindowAdapter implements ActionListener {
+    private AdminMenu adminMenu;
+
+    public AdminMenuEvent(AdminMenu adminMenu) {
+        this.adminMenu = adminMenu;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == adminMenu.getEmployeeInformationJbtn()){
+            System.out.println("사원정보 연결");
+        }
+        if (e.getSource() == adminMenu.getWorkAttendanceJbtn()){
+            System.out.println("근태관리 연결");
+        }
+        if (e.getSource() == adminMenu.getDocumentsJbtn()){
+            System.out.println("문서관리 연결");
+        }
+        if (e.getSource() == adminMenu.getCloseJbtn()){
+            adminMenu.dispose();
+        }
+        if (e.getSource() == adminMenu.getPasswordJbtn()){
+            System.out.println("비밀번호 변경 연결");
+        }
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        adminMenu.dispose();
+    }
 }
