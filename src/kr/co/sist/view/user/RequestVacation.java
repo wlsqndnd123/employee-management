ec997f215912677fb9f907e335b0fd04b026135d
OneStar90 <mrb8215@gmail.com>
2024-03-19 11:32:55 +0900

    Feat: 휴가신청 날짜 선택 기능 일부 구현

--- src/kr/co/sist/view/user/RequestVacation.java
+++ src/kr/co/sist/view/user/RequestVacation.java
@@ -4,6 +4,9 @@ import com.toedter.calendar.JDateChooser;
 import kr.co.sist.controller.event.RequestVacationEvent;
 
 import javax.swing.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Date;
 
 /**
  * Desc : 사원의 휴가 신청서 입력을 위한 view<br>
@@ -30,6 +33,7 @@ public class RequestVacation extends JFrame {
         createVacDate();
         createContentsArea();
         createConfirmButton();
+        createEvent();
 
         setBounds(300, 100, 650, 550);
         setVisible(true);
@@ -39,18 +43,18 @@ public class RequestVacation extends JFrame {
     /**
      * Desc : 휴가 시작일과 종료일을 설정하는 영역 생성
      */
-    public void createVacDate(){
+    public void createVacDate() {
         startDateTag = new JLabel("휴가 시작 일자");
         endDateTag = new JLabel("휴가 종료 일자");
 
         vacStartDate = new JDateChooser();
         vacEndDate = new JDateChooser();
 
-        startDateTag.setBounds(50,10,100,30);
-        endDateTag.setBounds(50,60,100,30);
+        startDateTag.setBounds(50, 10, 100, 30);
+        endDateTag.setBounds(50, 60, 100, 30);
 
-        vacStartDate.setBounds(150,10,150,30);
-        vacEndDate.setBounds(150,60,150,30);
+        vacStartDate.setBounds(150, 10, 150, 30);
+        vacEndDate.setBounds(150, 60, 150, 30);
 
         add(startDateTag);
         add(endDateTag);
@@ -62,11 +66,11 @@ public class RequestVacation extends JFrame {
     /**
      * Desc : 휴가 사유를 입력하는 영역 생성
      */
-    public void createContentsArea(){
+    public void createContentsArea() {
         vacContents = new JTextArea();
         contentsPad = new JScrollPane(vacContents);
 
-        contentsPad.setBounds(30, 100, 580,300);
+        contentsPad.setBounds(30, 100, 580, 300);
 
         add(contentsPad);
     }
@@ -74,19 +78,42 @@ public class RequestVacation extends JFrame {
     /**
      * Desc : 휴가 신청 및 취소버튼 생성
      */
-    public void createConfirmButton(){
+    public void createConfirmButton() {
         requestJbtn = new JButton("신청");
         cancelJbtn = new JButton("취소");
 
-        requestJbtn.setBounds(50,430,100,50);
-        cancelJbtn.setBounds(500,430,100,50);
+        requestJbtn.setBounds(50, 430, 100, 50);
+        cancelJbtn.setBounds(500, 430, 100, 50);
 
         add(requestJbtn);
         add(cancelJbtn);
     }
 
-    public void createEvent(){
+    /**
+     * Desc : JDateChooser의 이벤트 처리 ************************* 수정필요
+      */
+    public void createDateEvent(){
+        vacStartDate.getDateEditor().addPropertyChangeListener(evt -> {
+            if ("date".equals(evt.getPropertyName())) {
+                System.out.println(evt.getNewValue());
+            }
+        });
+
+        vacEndDate.getDateEditor().addPropertyChangeListener(evt -> {
+            if ("date".equals(evt.getPropertyName())) {
+                System.out.println(evt.getNewValue());
+            }
+        });
+
+        this.add(vacStartDate);
+        this.add(vacEndDate);
+    }
+
+    public void createEvent() {
         RequestVacationEvent requestVacationEvent = new RequestVacationEvent(this);
+        requestJbtn.addActionListener(requestVacationEvent);
+        cancelJbtn.addActionListener(requestVacationEvent);
+        createDateEvent();
     }
 
     public JButton getRequestJbtn() {
