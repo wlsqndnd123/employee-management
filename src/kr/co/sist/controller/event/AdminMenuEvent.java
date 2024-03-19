d27b9aa0116f024ddccfcae26586696692018efa
OneStar90 <mrb8215@gmail.com>
2024-03-19 09:33:17 +0900

    Feat: UserMenu 버튼 이벤트 추가

--- src/kr/co/sist/controller/event/AdminMenuEvent.java
+++ src/kr/co/sist/controller/event/AdminMenuEvent.java
@@ -7,6 +7,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+/**
+ * Desc : 관리자 로그인 메뉴 화면에 보이는 내용의 이벤트처리
+ * 작성일 : 2024.03.18
+ * 작성자 : 고한별
+ */
 public class AdminMenuEvent extends WindowAdapter implements ActionListener {
     private AdminMenu adminMenu;
 
@@ -14,6 +19,10 @@ public class AdminMenuEvent extends WindowAdapter implements ActionListener {
         this.adminMenu = adminMenu;
     }
 
+    /**
+     * Desc : 각각의 버튼에 연결되는 이벤트 관리
+     * @param e the event to be processed
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == adminMenu.getEmployeeInformationJbtn()){
@@ -26,15 +35,26 @@ public class AdminMenuEvent extends WindowAdapter implements ActionListener {
             System.out.println("문서관리 연결");
         }
         if (e.getSource() == adminMenu.getCloseJbtn()){
-            adminMenu.dispose();
+            closeFrame();
         }
         if (e.getSource() == adminMenu.getPasswordJbtn()){
             System.out.println("비밀번호 변경 연결");
         }
     }
 
+    /**
+     * Desc : 타이틀 바의 x 누를 경우 창 닫기 기능
+     * @param e the event to be processed
+     */
     @Override
     public void windowClosing(WindowEvent e) {
+        closeFrame();
+    }
+
+    /**
+     * Desc : 창 닫기 *******구현필요
+     */
+    public void closeFrame(){
         adminMenu.dispose();
     }
 }
