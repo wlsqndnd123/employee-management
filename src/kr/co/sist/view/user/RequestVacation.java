ad408a48b50804b9ac56819e5e99c373f034d8c1
OneStar90 <mrb8215@gmail.com>
2024-03-22 23:02:07 +0900

    Feat: 연차 바인드변수 적용 실패

--- src/kr/co/sist/view/user/RequestVacation.java
+++ src/kr/co/sist/view/user/RequestVacation.java
@@ -6,6 +6,7 @@ import kr.co.sist.controller.event.RequestVacationEvent;
 import javax.swing.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -23,6 +24,11 @@ public class RequestVacation extends JFrame {
     private JScrollPane contentsPad;
     private JButton requestJbtn;
     private JButton cancelJbtn;
+    private String startDate;
+    private String endDate;
+    private Date sDate;
+    private Date eDate;
+
 
     /**
      * Desc : 휴가 신청 main frame 구현
@@ -94,11 +100,25 @@ public class RequestVacation extends JFrame {
      * Desc : JDateChooser의 이벤트 처리
       */
     public void createDateEvent(){
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
         PropertyChangeListener dateChangeListener = evt -> {
             if ("date".equals(evt.getPropertyName())) {
-                System.out.println(simpleDateFormat.format(evt.getNewValue()));
+                if (evt.getSource() == vacStartDate.getDateEditor()) {
+                    startDate = simpleDateFormat.format(vacStartDate.getDate());
+                    try {
+                        simpleDateFormat.parse(startDate);
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else if (evt.getSource() == vacEndDate.getDateEditor()) {
+                    endDate = simpleDateFormat.format(vacEndDate.getDate());
+                    try {
+                        simpleDateFormat.parse(endDate);
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
         };
 
@@ -125,6 +145,27 @@ public class RequestVacation extends JFrame {
 
     public JButton getCancelJbtn() {
         return cancelJbtn;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public JTextArea getVacContents() {
+        return vacContents;
+    }
+
+
+    public Date getsDate() {
+        return sDate;
+    }
+
+    public Date geteDate() {
+        return eDate;
     }
 
     public static void main(String[] args) {
