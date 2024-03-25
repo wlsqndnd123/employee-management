f79ac3f79a049bfdf6cc65b168102cff22e79355
OneStar90 <mrb8215@gmail.com>
2024-03-25 19:34:19 +0900

    Refactor: UpdateTelNumber

--- src/kr/co/sist/view/user/UpdateTelNumber.java
+++ src/kr/co/sist/view/user/UpdateTelNumber.java
@@ -1,6 +1,7 @@
 package kr.co.sist.view.user;
 
 import kr.co.sist.controller.event.UpdateTelNumberEvent;
+import kr.co.sist.view.util.JFrameComponent;
 import kr.co.sist.vo.EmpInfoVO;
 
 import javax.swing.*;
@@ -9,129 +10,57 @@ public class UpdateTelNumber extends JFrame {
     private JTextField tfEmpno, tfName, tfPosition, tfJob, tfTel, tfDep;
     private JTextField inputJtTel;
     private JButton goHome, jbtnsave, jbtnexcPw;
-    private JLabel jlInfo, jlNewTel;
-
-
+    private JLabel jlNewTel, jlEmpno, jlName, jlPosition, jlJob, jlTel, jlDept;
     private EmpInfoVO eVO;
 
     public UpdateTelNumber(EmpInfoVO eVO) {
         super("사원창(정보변경)");
-
         setLayout(null);
+        this.eVO = eVO;
 
+        createLabel();
+        createTextField();
+        createButton();
+        createEvent();
 
-        //창에 출력하는 사원 정보
-        JLabel jlEmpno = new JLabel("사원번호");
-        JLabel jlName = new JLabel("사원이름");
-        JLabel jlPosition = new JLabel("직급");
-        JLabel jlJob = new JLabel("직무");
-        JLabel jlTel = new JLabel("내선번호");
-        JLabel jlDept = new JLabel("부서");
-        tfEmpno = new JTextField(10);
-        tfEmpno.setText(String.valueOf(eVO.getEmpno()));
-        tfName = new JTextField(10);
-        tfName.setText(eVO.getName());
-        tfPosition = new JTextField(10);
-        tfPosition.setText(eVO.getPosition());
-        tfJob = new JTextField(10);
-        tfJob.setText(eVO.getJob());
-        tfTel = new JTextField(10);
-        tfTel.setText(eVO.getTel());
-        tfDep = new JTextField(10);
-        tfDep.setText(eVO.getDept());
-        tfEmpno.setEditable(false);
-        tfName.setEditable(false);
-        tfPosition.setEditable(false);
-        tfJob.setEditable(false);
-        tfTel.setEditable(false);
-        tfDep.setEditable(false);
-        //사원정보 표시창 위치조정
-        jlEmpno.setBounds(100, 65, 60, 30);
-        tfEmpno.setBounds(175, 65, 100, 30);
-        jlName.setBounds(100, 120, 60, 30);
-        tfName.setBounds(175, 120, 100, 30);
-        jlPosition.setBounds(100, 175, 60, 30);
-        tfPosition.setBounds(175, 175, 100, 30);
-
-        jlJob.setBounds(300, 65, 60, 30);
-        tfJob.setBounds(370, 65, 100, 30);
-        jlTel.setBounds(300, 120, 60, 30);
-        tfTel.setBounds(370, 120, 100, 30);
-        jlDept.setBounds(300, 175, 60, 30);
-        tfDep.setBounds(370, 175, 100, 30);
-
-        jlNewTel = new JLabel("변경 할 내선번호");
-        inputJtTel = new JTextField(10);
-
-        jlNewTel.setBounds(250, 250, 100, 30);
-        inputJtTel.setBounds(250, 280, 100, 30);
-        add(jlNewTel);
-        add(inputJtTel);
-
-        //라벨
-        goHome = new JButton("돌아가기");
-        jbtnsave = new JButton("저장");
-        jbtnexcPw = new JButton("비밀번호 변경");
-        //액션리스너 등록
-        UpdateTelNumberEvent evt = new UpdateTelNumberEvent(this);
-        jbtnsave.addActionListener(evt);
-        goHome.addActionListener(evt);
-        jbtnexcPw.addActionListener(evt);
-
-        jbtnsave.setBounds(80, 350, 150, 30);
-        add(jbtnsave);
-
-        goHome.setBounds(250, 350, 150, 30);
-        add(goHome);
-
-        jbtnexcPw.setBounds(420, 350, 150, 30);
-        add(jbtnexcPw);
-
-//	        tfEmpno = new JTextField(10);
-//	        tfEmpno.setText(String.valueOf(eVO.getEmpno()));
-//	        tfName= new JTextField(10);
-//	        tfName.setText(eVO.getName());
-//	        tfPosition= new JTextField(10);
-//	        tfPosition.setText(eVO.getPosition());
-//	        tfJob = new JTextField(10);
-//	        tfJob.setText(eVO.getJob());
-//	        tfTel= new JTextField(10);
-//	        tfTel.setText(eVO.getTel());
-//	        tfDep= new JTextField(10);
-//	        tfDep.setText(eVO.getDept());
-//	        tfEmpno.setEditable(false);
-//	        tfName.setEditable(false);
-//	        tfPosition.setEditable(false);
-//	        tfJob.setEditable(false);
-//	        tfTel.setEditable(false);
-//	        tfDep.setEditable(false);
-        //본인의 개인정보를 받아서 JTF에 뿌려야함
-        //loginVO의 EMPNO?
-        //TEL 입력받는 부분
-        add(jlEmpno);
-        add(tfEmpno);
-        add(jlName);
-
-        add(tfName);
-        add(jlPosition);
-        add(tfPosition);
-
-        add(jlJob);
-        add(tfJob);
-
-        add(jlTel);
-        add(tfTel);
-
-        add(jlDept);
-        add(tfDep);
-
-        setSize(500, 600);
         setBounds(300, 100, 650, 550);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
     }
 
+    private void createButton(){
+        goHome = JFrameComponent.createButton(getContentPane(),"돌아가기",250, 350, 150, 30);
+        jbtnsave = JFrameComponent.createButton(getContentPane(),"저장",80, 350, 150, 30);
+        jbtnexcPw = JFrameComponent.createButton(getContentPane(),"비밀번호 변경",420, 350, 150, 30);
+    }
+
+    private void createTextField(){
+        tfEmpno = JFrameComponent.createTextField(getContentPane(),String.valueOf(eVO.getEmpno()),175, 65, 100, 30,false);
+        tfName = JFrameComponent.createTextField(getContentPane(),eVO.getName(),175, 120, 100, 30,false);
+        tfPosition = JFrameComponent.createTextField(getContentPane(),eVO.getPosition(),175, 175, 100, 30,false);
+        tfJob = JFrameComponent.createTextField(getContentPane(),eVO.getJob(),370, 65, 100, 30,false);
+        tfTel = JFrameComponent.createTextField(getContentPane(),eVO.getTel(),370, 120, 100, 30,false);
+        tfDep = JFrameComponent.createTextField(getContentPane(),eVO.getDept(),370, 175, 100, 30,false);
+        inputJtTel = JFrameComponent.createTextField(getContentPane(),250, 280, 100, 30);
+    }
+
+    private void createLabel(){
+        jlEmpno = JFrameComponent.createLabel(getContentPane(),"사원번호",100, 65, 60, 30);
+        jlName = JFrameComponent.createLabel(getContentPane(),"사원이름",100, 120, 60, 30);
+        jlPosition = JFrameComponent.createLabel(getContentPane(),"직급",100, 175, 60, 30);
+        jlJob = JFrameComponent.createLabel(getContentPane(),"직무",300, 65, 60, 30);
+        jlTel = JFrameComponent.createLabel(getContentPane(),"내선번호",300, 120, 60, 30);
+        jlDept = JFrameComponent.createLabel(getContentPane(),"부서",300, 175, 60, 30);
+        jlNewTel = JFrameComponent.createLabel(getContentPane(),"변경 할 내선번호",250, 250, 100, 30);
+    }
+
+    private void createEvent(){
+        UpdateTelNumberEvent evt = new UpdateTelNumberEvent(this);
+
+        jbtnsave.addActionListener(evt);
+        goHome.addActionListener(evt);
+        jbtnexcPw.addActionListener(evt);
+    }
 
     /**
      * @return the goHome
@@ -143,7 +72,6 @@ public class UpdateTelNumber extends JFrame {
     public JTextField getInputJtTel() {
         return inputJtTel;
     }
-
 
     public JButton getJbtnexcPw() {
         return jbtnexcPw;
