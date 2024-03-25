6015f59aeecbbb5fbe6d9d75e35600d7ce99af74
OneStar90 <mrb8215@gmail.com>
2024-03-25 18:45:42 +0900

    Refactor: UpdatePassword

--- src/kr/co/sist/view/common/UpdatePassword.java
+++ src/kr/co/sist/view/common/UpdatePassword.java
@@ -1,112 +1,86 @@
 package kr.co.sist.view.common;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.SQLException;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-
 import kr.co.sist.controller.event.UpdatePasswordEvent;
-import kr.co.sist.dao.UpdatePasswordDAO;
+import kr.co.sist.view.util.JFrameComponent;
 import kr.co.sist.vo.LoginVO;
 import kr.co.sist.vo.UpdatePasswordVO;
 
-public class UpdatePassword extends JFrame{
-//	private JLabel jlCurrentPw, jlUpdatePw;
-	private JTextField jtfUpdatePw;
-	private JTextField jtfCurrentPw;
-	private JButton UpdateButton, ExitButton;
-	private String password;
+import javax.swing.*;
 
-	private LoginVO lVO;
-	
-	public UpdatePassword() {
-	}
+public class UpdatePassword extends JFrame {
+    private JLabel jlUpdatePw, jlCurrentPw;
+    private JTextField jtfUpdatePw;
+    private JTextField jtfCurrentPw;
+    private JButton UpdateButton, ExitButton;
+    private String password;
+    private LoginVO lVO;
+    private static UpdatePasswordVO upVO;
 
-	private static UpdatePasswordVO upVO;
-	//private static LoginVO lVO = new LoginVO("240001","tlqkf");
+    public UpdatePassword() {
+    }
 
+    public UpdatePassword(LoginVO lVO) {
+        super("비밀번호 변경");
+        setLayout(null);
+        this.lVO = lVO;
 
-	public UpdatePassword(LoginVO lVO) {
-		super("비밀번호 변경");
-		
-//		lVO.setEmp_no("240001");
-//		lVO.setPassword("tlqkf");
-//		
-		this.lVO=lVO;
-		
-		//입력받는 변경할 비밀번호
-		JLabel jlUpdatePw = new JLabel("변경할 비밀번호");
-		jtfUpdatePw = new JTextField();
+        createLabel();
+        createTextField();
+		createButton();
+        createEvent();
 
-		//창에 출력하는 현재 비밀번호
-		JLabel jlCurrentPw = new JLabel("현재 비밀번호");
-		jtfCurrentPw = new JTextField();
-		jtfCurrentPw.setText(lVO.getPassword());
-		jtfCurrentPw.setEditable(false);
-		
-		//버튼
-		UpdateButton = new JButton("변경");
-		ExitButton = new JButton("취소");
-		
-		//수동 배치
-		jlCurrentPw.setBounds(50, 80, 90, 40);
-		jtfCurrentPw.setBounds(150, 80, 200, 40);
-		jlUpdatePw.setBounds(50, 130, 90, 40);
-		jtfUpdatePw.setBounds(150, 130, 200, 40);
-		
-		UpdateButton.setBounds(170, 200, 60, 30);
-		ExitButton.setBounds(260, 200, 60, 30);
-		
-		
-		//컴포넌트 추가
-		add(jlCurrentPw);
-		add(jlUpdatePw);
-		add(jtfUpdatePw);
-		add(UpdateButton);
-		add(ExitButton);
+        setBounds(300, 120, 500, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }//UpdatePassword
 
-		
-		//액션리스너
-		UpdatePasswordEvent evt = new UpdatePasswordEvent(this);
-		UpdateButton.addActionListener(evt);
-		ExitButton.addActionListener(evt);
-		
-		setLayout(null);
-		setBounds(300,120,500,400);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true); 
-		
-	}//UpdatePassword
-	public JTextField getJtfUpdatePw() {
-		return jtfUpdatePw;
-	}
-	public JTextField getJtfCurrentPw() {
-		return jtfCurrentPw;
-	}
-	public JButton getUpdateButton() {
-		return UpdateButton;
-	}
-	public JButton getExitButton() {
-		return ExitButton;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public static UpdatePasswordVO getUpVO() {
-		return upVO;
-	}
-	
-	public LoginVO getLoginVO() {
-		return lVO;
-	}
-	
-	public static void main(String[] args) {
-		LoginVO lv=new LoginVO("240000", "1111");
-		new UpdatePassword(lv );
-	}
+    private void createLabel(){
+        jlUpdatePw = JFrameComponent.createLabel(getContentPane(),"변경할 비밀번호",50, 130, 90, 40);
+        jlCurrentPw = JFrameComponent.createLabel(getContentPane(),"현재 비밀번호",50, 80, 90, 40);
+    }
+
+    private void createTextField(){
+        jtfUpdatePw = JFrameComponent.createTextField(getContentPane(),150, 130, 200, 40);
+        jtfCurrentPw = JFrameComponent.createTextField(getContentPane(),lVO.getPassword(),150, 80, 200, 40,false);
+    }
+
+    private void createButton(){
+        UpdateButton = JFrameComponent.createButton(getContentPane(),"변경",170, 200, 60, 30);
+        ExitButton = JFrameComponent.createButton(getContentPane(),"취소",260, 200, 60, 30);
+    }
+
+    private void createEvent(){
+        UpdatePasswordEvent evt = new UpdatePasswordEvent(this);
+
+        UpdateButton.addActionListener(evt);
+        ExitButton.addActionListener(evt);
+    }
+
+    public JTextField getJtfUpdatePw() {
+        return jtfUpdatePw;
+    }
+
+    public JTextField getJtfCurrentPw() {
+        return jtfCurrentPw;
+    }
+
+    public JButton getUpdateButton() {
+        return UpdateButton;
+    }
+
+    public JButton getExitButton() {
+        return ExitButton;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public static UpdatePasswordVO getUpVO() {
+        return upVO;
+    }
+
+    public LoginVO getLoginVO() {
+        return lVO;
+    }
 }//class
