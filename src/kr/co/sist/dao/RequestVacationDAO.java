3a2b9596820341ebea47023b2f14f7e6f6264184
OneStar90 <mrb8215@gmail.com>
2024-03-26 17:15:26 +0900

    Fix: 사원 문서 조회 수정

--- src/kr/co/sist/dao/RequestVacationDAO.java
+++ src/kr/co/sist/dao/RequestVacationDAO.java
@@ -1,10 +1,12 @@
 package kr.co.sist.dao;
 
 import kr.co.sist.util.DbConnection;
+import kr.co.sist.vo.DocumentVO;
 import kr.co.sist.vo.VacationVO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -30,37 +32,53 @@ public class RequestVacationDAO {
         return requestVacationDAO;
     }
 
-    /**
-     * Desc : view에 필요한 Data 호출
-     *
-     * @return : 관련 데이터 list
-     * @throws SQLException
-     */
-    public void vacationData(VacationVO vVO) throws SQLException {
+    public int searchMaxDocNum() throws SQLException {
+        int docNum = 0;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet =null;
+        try {
+            connection = DbConnection.getCon();
+
+            String searchMaxNum =
+                    "	select nvl(max(DOC_NO),0)+1 DOC_NO from BUSINESS_LOG	";
+            preparedStatement =connection.prepareStatement(searchMaxNum);
+            resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()) {
+                docNum = resultSet.getInt("DOC_NO");
+            }
+
+        }finally {
+            DbConnection.dbClose(resultSet, preparedStatement, connection);
+        }
+        return docNum;
+    }
+
+    public void insertBusinessLog(int docno, VacationVO vVO) throws SQLException {
+
         Connection connection = null;
         PreparedStatement preparedStatement = null;
 
-
         try {
             connection = DbConnection.getCon();
-            String insertVacationLog = "insert into BUSINESS_LOG(doc_no, title, emp_no, grp_code, code, work_log, grp_code2, code2, start_date, end_date) " +
-                    " values ('000002110','연차신청서',240004,'WORK',5,'ㅅㅂ','APPR',1,?,?) ";
 
-            preparedStatement = connection.prepareStatement(insertVacationLog);
-//            resultSet = preparedStatement.executeQuery();
+            String insertDoc = " insert into BUSINESS_LOG " +
+                    " (DOC_NO, TITLE, EMP_NO, GRP_CODE, CODE, WORK_LOG, GRP_CODE2, CODE2, start_date, end_date)  " +
+                    " values(?,'연차신청서',?,'WORK',5,?,'APPR',1,?,?) ";
 
-//            preparedStatement.setString(1,vacationVO.getDocNo());
-//            preparedStatement.setString(2, vacationVO.getWorkLog());
-            preparedStatement.setDate(1, vVO.getStartDate());
-            preparedStatement.setDate(2, vVO.getEndDate());
+            preparedStatement = connection.prepareStatement(insertDoc);
+
+            preparedStatement.setInt(1,docno);
+            preparedStatement.setInt(2, vVO.getEmpNo());
+            preparedStatement.setString(3, vVO.getWorkLog());
+            preparedStatement.setDate(4, vVO.getStartDate());
+            preparedStatement.setDate(5, vVO.getEndDate());
 
             preparedStatement.executeUpdate();
+
         } finally {
             DbConnection.dbClose(null, preparedStatement, connection);
         }
-    }
-
-    public static void main(String[] args) {
-
     }
 }
