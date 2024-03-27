f1dc66250773def7e533321c5660476ea627eb46
dowon <dowon1235@naver.com>
2024-03-27 14:05:22 +0900

    login 주석 삭제

--- src/kr/co/sist/dao/LoginDAO.java
+++ src/kr/co/sist/dao/LoginDAO.java
@@ -55,12 +55,6 @@ public class LoginDAO {
         }
     }
 
-//	public static LoginDAO getInstance() {
-//		if(lDAO == null) {
-//			lDAO = new LoginDAO();
-//		}//end if
-//		return lDAO;
-//	}//end LoginDAO
 
     public LoginVO confirmUser(String emp_no) {
         LoginVO lVO = null;
@@ -103,58 +97,5 @@ public class LoginDAO {
     public static void main(String[] args) {
     }
 }
-//	
-//	public void loginClik() {
-//		setInputInfo(); // 입력 받은 아이디와 비밀번호를 설정합니다.
-//	    if (confirmUser() != null) { // confirmUser() 메서드를 통해 사용자 정보가 확인되면
-//	        LoginVO lVO = confirmUser(); // 사용자 정보를 가져옵니다.
-//	        String authCode = lVO.getAuthCode(); // 사용자의 권한 코드를 가져옵니다. (예시)
-//	        
-//	        // 권한 코드에 따라 다른 메뉴를 띄웁니다.
-//	        if (authCode.equals("ADMIN")) { // 관리자인 경우
-//	            SwingUtilities.invokeLater(AdminMenu::new);
-//	        } else if (authCode.equals("USER")) { // 사용자인 경우
-//	            SwingUtilities.invokeLater(UserMenu::new);
-//	        } else { // 권한 코드가 잘못된 경우
-//	            JOptionPane.showMessageDialog(null, "권한이 없는 사용자입니다.");
-//	        }
-//	    } else { // 사용자 정보가 확인되지 않은 경우
-//	        JOptionPane.showMessageDialog(null, "사용자 정보를 확인할 수 없습니다.");
-//	    }
-//	}//loginClik
-//
-//	
-//	
-//	
-//	public String getLogInfo() throws SQLException{
-//		String authCode = "";
-//
-//        Connection con = null;
-//        PreparedStatement pstmt = null;
-//        ResultSet rs = null;
-//
-//        try {
-//            con = DbConnection.getCon();
-//
-//            // 아이디와 비밀번호로 인증코드를 가져오는 쿼리 실행
-//            String query = "SELECT auth_code FROM user_auth WHERE emp_no = ? AND pass = ?";
-//            pstmt = con.prepareStatement(query);
-//            
-//            String emp_no = null;
-//            String password = null;
-//			pstmt.setString(1, emp_no);
-//			pstmt.setString(2, password);
-//			
-//            rs = pstmt.executeQuery();
-//
-//            if (rs.next()) {
-//                authCode = rs.getString("auth_code");
-//            }
-//        } finally {
-//            DbConnection.dbClose(rs, pstmt, con);
-//        }
-//
-//        return authCode;
-//    }
-	
+
 
